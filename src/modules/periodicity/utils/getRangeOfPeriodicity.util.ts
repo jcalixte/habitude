3fa515c9bf833a768eaa7e69ec0d7e8ab3189985
Julien Calixte <juliencalixte@gmail.com>
2020-12-12 14:28:33 +0100

    :sparkles: (periodicity) add a lib to get range from periodicity

---
+++ src/modules/periodicity/utils/getRangeOfPeriodicity.util.ts
@@ -0,0 +1,45 @@
+import { Periodicity } from '@/data/constants/Periodicity.constant'
+import {
+  startOfDay,
+  startOfWeek,
+  startOfMonth,
+  startOfYear,
+  endOfDay,
+  endOfWeek,
+  endOfMonth,
+  endOfYear
+} from 'date-fns'
+
+const getStartOfPeriodicity = (periodicity: Periodicity, date: Date) => {
+  switch (periodicity) {
+    case 'daily':
+      return startOfDay(date)
+    case 'weekly':
+      return startOfWeek(date)
+    case 'monthly':
+      return startOfMonth(date)
+    case 'yearly':
+      return startOfYear(date)
+  }
+}
+
+const getEndOfPeriodicity = (periodicity: Periodicity, date: Date) => {
+  switch (periodicity) {
+    case 'daily':
+      return endOfDay(date)
+    case 'weekly':
+      return endOfWeek(date)
+    case 'monthly':
+      return endOfMonth(date)
+    case 'yearly':
+      return endOfYear(date)
+  }
+}
+
+export const getRangeOfPeriodicity = (
+  periodicity: Periodicity,
+  date: Date
+): [Date, Date] => [
+  getStartOfPeriodicity(periodicity, date),
+  getEndOfPeriodicity(periodicity, date)
+]
